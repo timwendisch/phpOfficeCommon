b9485b695ddc80b34e1faf31dc71c796aa610d53
Tim Wendisch <info@wendisch-media.de>
2019-04-01 23:12:24 +0200

    initial upload

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+/***************************************************************
+ * Extension Manager/Repository config file for ext "phpOfficeCommon".
+ *
+ * Auto generated 01-04-2019 22:12
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Library phpOfficeCommon',
+    'description' => 'just a wrapper for the famous library. The original sources taken from https://github.com/PHPOffice',
+    'description' => '',
+    'category' => 'misc',
+    'shy' => 0,
+    'version' => '0.2.9',
+    'dependencies' => 'cms',
+    'conflicts' => '',
+    'priority' => '',
+    'loadOrder' => '',
+    'module' => '',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearcacheonload' => 0,
+    'lockType' => '',
+    'author' => 'Mark Baker, Franck Lefevre, Tim Wendisch',
+    'CGLcompliance' => '',
+    'CGLcompliance_note' => '',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '8.7.0-8.7.99',
+            'php' => '5.6.0-7.2.99'
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+    'suggests' => array(
+    ),
+);
